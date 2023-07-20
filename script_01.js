07b9c2bfdc7b3e04c082930794ed19671921a695
Rekxis <Rekxis@gmx.de>
2023-07-20 09:03:46 +0200

    draft with key-value

--- script_01.js
+++ script_01.js
@@ -8,6 +8,9 @@
 // "E" (exclamation) --> "!"  Ich bin!
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
+const GAP = " ";
+const PUNCTUATION = {S: '.', Q: '?', E: '!'}
+const ERROR_PUNC = "FEHLER! Satzzeichen nicht erkannt."
 
 
 /*** Funktion mit Array als Parameter */
@@ -15,27 +18,36 @@
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei",
+                    "diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+output(getSentence(["Ich","bin","Max"],"A"));
 
 function getSentence(arr,op) {
-    const GAP = " ";
-    
-    return arr.join(GAP) + getPunctuation(op);
+    // return arr.join(GAP) + getPunctuation(op);
+    if (op in PUNCTUATION) {
+        return arr.join(GAP) + PUNCTUATION[op];
+    } else {
+        return ERROR_PUNC
+    }
 }
 
-function getPunctuation(op) {
-    if (op == "S") {
-        op = "."
-    } else if (op == "Q") {
-        op = "?"
-    } else {
-        op = "!"
-    }
-    return op;
-}
+// function getPunctuation(op) {
+//     if (op == "S") {
+//         op = "."
+//     } else if (op == "Q") {
+//         op = "?"
+//     } else if (op == "E"){
+//         op = "!"
+//     } else {
+//         op = "";
+//     }
+//     return op;
+// }
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
+
 function output(outputData) {
     console.log(outputData);
 }
