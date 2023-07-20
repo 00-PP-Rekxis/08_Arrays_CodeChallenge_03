3b5f2d50180c2719ad623165b65398efa9f5452f
Rekxis <Rekxis@gmx.de>
2023-07-20 09:11:13 +0200

    working challenge

--- script_01.js
+++ script_01.js
@@ -20,33 +20,15 @@ output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei",
                     "diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
-
-output(getSentence(["Ich","bin","Max"],"A"));
+output(getSentence(["Ich","bin","Max"],"A")); // ERROR_PUNC
 
 function getSentence(arr,op) {
-    // return arr.join(GAP) + getPunctuation(op);
     if (op in PUNCTUATION) {
         return arr.join(GAP) + PUNCTUATION[op];
     } else {
         return ERROR_PUNC
     }
 }
-
-// function getPunctuation(op) {
-//     if (op == "S") {
-//         op = "."
-//     } else if (op == "Q") {
-//         op = "?"
-//     } else if (op == "E"){
-//         op = "!"
-//     } else {
-//         op = "";
-//     }
-//     return op;
-// }
-
-// Modul: Ausgabe in Konsole : Test
-// output("hi"); 
 
 function output(outputData) {
     console.log(outputData);
